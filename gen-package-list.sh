6f725604d0a7b36528dbff48b0ce5b624801f9c9
Ryan Johnson <ryanj00a@gmail.com>
2019-09-15 15:53:11 -0700

    update package list and script

--- gen-package-list.sh
+++ gen-package-list.sh
@@ -4,15 +4,11 @@ set -e
 cd "$(dirname "$0")"
 
 explicit='package-list/explicit.txt'
-everything_verbose='package-list/everything-verbose.txt'
 everything='package-list/everything.txt'
 
 printf '==== Generated with "pacman -Qqe" ====\n\n' > $explicit
 pacman -Qqe >> $explicit
 
-printf '==== Generated with "pacman -Qq" ====\n\n' > $everything
-pacman -Qq >> $everything
-
-printf '==== Generated with "pacman -Qie" ====\n\n' > $everything_verbose
-pacman -Qi >> $everything_verbose
+printf '==== Generated with "pacman -Q" ====\n\n' > $everything
+pacman -Q >> $everything
 
