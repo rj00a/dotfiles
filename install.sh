efd0a3a7e59a97ea62a12110a1af405899ae3362
Ryan Johnson <ryanj00a@gmail.com>
2019-09-15 21:31:06 -0700

    switch to vim-plug from pathogen. This obviates the need for submodules to manage vim plugins.

--- install.sh
+++ install.sh
@@ -2,28 +2,29 @@
 
 cd "$(dirname "$0")"
 
-cfg () {
+l () {
 	ln -sf $(pwd)/files/"$1" "$2"
 }
 
-cfg .vim/ ~/
-cfg i3/ ~/.config/
-cfg .bashrc ~/
-cfg .bash_profile ~/
-cfg .xinitrc ~/.xinitrc
-cfg gtk-3.0 ~/.config/
-cfg .gtkrc-2.0 ~/
-cfg .Xresources ~/
-cfg .gitconfig ~/
-cfg alacritty.yml ~/.config/
-cfg vlcrc ~/.config/vlc/
-cfg .nethackrc ~/
-cfg .inputrc ~/
-cfg .gdbinit.d ~/
-cfg gdb-dashboard/.gdbinit ~/
+l .vim/ ~/
+l i3/ ~/.config/
+l .bashrc ~/
+l .bash_profile ~/
+l .xinitrc ~/.xinitrc
+l gtk-3.0 ~/.config/
+l .gtkrc-2.0 ~/
+l .Xresources ~/
+l .gitconfig ~/
+l alacritty.yml ~/.config/
+l vlcrc ~/.config/vlc/
+l .nethackrc ~/
+l .inputrc ~/
+l .gdbinit.d ~/
+l gdb-dashboard/.gdbinit ~/
+l vim-plug/plug.vim ~/.vim/autoload/
 
 sudo systemctl enable NetworkManager
 sudo systemctl enable org.cups.cupsd
 
-# Run `nitrogen ~/install/` to configure wallpaper.
+# Run `nitrogen ~/shared/` to configure wallpaper.
 
