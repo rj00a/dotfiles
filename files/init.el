cc183168cd8b329fa9fade08a236bc464cf0ff31
Ryan Johnson <ryanj00a@gmail.com>
2020-05-02 19:58:33 -0700

    update

--- files/init.el
+++ files/init.el
@@ -1,5 +1,5 @@
 ;; TODO: Rust mode stuff
-;; TODO: Try solarized-wombat-dark-theme.el
+;; TODO: language server?
 ;; TODO: Customize gdb?
 ;; TODO: eshell-previous-input, eshell-next-input
 
@@ -12,15 +12,13 @@
 ;; Update list of packages asynchronously
 (package-refresh-contents t)
 
-;; naysayer-theme
 (setq my-packages
       '(evil
         evil-leader
         centered-cursor-mode
-        gruvbox-theme
-        solarized-theme
         hl-todo
         evil-collection
+        nord-theme
         powerline
         fish-mode
         magit
@@ -37,7 +35,7 @@
   (require pkg))
 
 ;; Load theme without asking for confirmation
-(load-theme 'solarized-wombat-dark t)
+(load-theme 'nord t)
 
 ;; Set the default font
 (set-face-attribute 'default nil
@@ -47,7 +45,10 @@
                     :width 'normal)
 
 ;; Make the cursor green.
-;(set-cursor-color "#00ff00")
+(set-cursor-color "#00ff00")
+
+;; Make the cursor white.
+;(set-cursor-color "#ffffff")
 
 ;; Make the cursor never stop blinking.
 (setq blink-cursor-blinks 0)
@@ -62,6 +63,7 @@
   "f" 'ido-find-file
   "d" 'ido-dired
   "D" 'cd
+  "m" 'mkdir
   "b" 'ido-switch-buffer
   "l" 'buffer-menu
   "k" 'kill-current-buffer
@@ -73,10 +75,18 @@
   "c" 'calculator
   "e" 'eval-expression
   "0" 'delete-trailing-whitespace
+  "9" (lambda () ;; Toggle flyspell
+        (interactive)
+        (if (bound-and-true-p flyspell-mode)
+            (turn-off-flyspell)
+          (progn
+            (flyspell-mode 1)
+            (flyspell-buffer))))
   "i" (lambda () ;; Open init.el
         (interactive)
         (find-file user-init-file))
-  "I" 'eval-buffer)
+  "I" 'eval-buffer
+  "Q" 'kill-emacs)
 
 ;; Enable evil mode.
 (evil-mode 1)
@@ -87,7 +97,23 @@
 (evil-collection-init)
 
 ;; Make searching with / or ? not wrap around.
-(setq evil-search-wrap nil)
+'(setq evil-search-wrap nil)
+
+;; When isearching, don't delay before highlighting all matches.
+(setq lazy-highlight-initial-delay 0)
+
+;(evil-define-key nil 
+;  (kbd "ESC") 'evil-ex-nohilight)
+
+;(global-set-key (kbd "<escape>") 'evil-ex-nohighlight)
+
+(evil-global-set-key
+ 'evil-normal-state-map
+ (kbd "<escape>")
+ 'evil-ex-nohighlight)
+
+;; Change '/' to use evil's search, instead of being a wrapper around isearch.
+(evil-select-search-module 'evil-search-module 'evil-search)
 
 ;; Make evil treat emacs symbols as words.
 ;; For example, "foo-bar" is a symbol in lisp, and "foo_bar" is a symbol in C.
@@ -104,25 +130,36 @@
 (global-centered-cursor-mode 1)
 
 ;; Eable Ido mode
+;; Don't use (setq ido-everywhere t) because I want find-file to do
+;; its usual thing and make it easier to create new files.
 (setq ido-enable-flex-matching t
-      ido-everywhere t
+      ;;ido-everywhere t
       ido-cannot-complete-command 'ido-next-match)
 (ido-mode 1)
 
 ;; Disable tool bar.
 (tool-bar-mode -1)
 
-;; Disable menu bar
+;; Disable menu bar.
 (menu-bar-mode -1)
 
 ;; Disable scroll bar.
 (toggle-scroll-bar -1)
 
 ;; Enables line numbers.
-(global-linum-mode 1)
+;(global-linum-mode 1)
 
 ;; Highlight TODOs and similar keywords.
 (global-hl-todo-mode 1)
+
+;; Better word wrapping behavior.
+(global-visual-line-mode 1)
+
+;; Auto revert files.
+(global-auto-revert-mode 1)
+
+;; Auto revert dired
+(setq global-auto-revert-non-file-buffers t)
 
 ;; Get whitespace mode to show only the important stuff.
 (setq whitespace-style
@@ -138,9 +175,20 @@
 ;; (This is the same as C-g)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+;; Dired tweaks
+(put 'dired-find-alternate-file 'disabled nil) ;; Disable warning on use.
+(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) ;; Make 'RET' an alias for 'a'
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ;; Don't create a new buffer with '^'
+
 ;; Show matching paren on hover, with no delay (must be in this order)
 (setq show-paren-delay 0)
+
 (show-paren-mode 1)
+;(require 'paren)
+(set-face-background 'show-paren-match (face-background 'default))
+(set-face-foreground 'show-paren-match "#def")
+(set-face-attribute 'show-paren-match nil :weight 'extra-bold)
+(set-face-underline 'show-paren-match t)
 
 ;; Change the C formatting style for c-like languages.
 ;; I would rather shoot myself than use the default GNU style.
@@ -150,16 +198,19 @@
 ;; Set the default powerline theme
 (powerline-default-theme)
 
-;; Mousing over a window causes it to gain focus, without having to click on it.
+;; Mousing over a window causes it to gain focus without having to click on it.
 (setq mouse-autoselect-window t)
 
 ;; Spaces > Tabs
 (setq-default indent-tabs-mode nil)
 
+;; Tab = 4 spaces
+(setq-default tab-width 4)
+
 ;; Don't show the startup file
 (setq inhibit-startup-screen t)
 
-;; Empty scratch buffer with fundamental-mode.
+;; Empty scratch buffer and enter with fundamental-mode.
 (setq initial-scratch-message nil
       initial-major-mode 'fundamental-mode)
 
@@ -189,23 +240,3 @@
              (let ((buffer "*Completions*"))
                (and (get-buffer buffer)
                     (kill-buffer buffer)))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3c3836" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
- '(custom-safe-themes
-   (quote
-    ("b89ae2d35d2e18e4286c8be8aaecb41022c1a306070f64a66fd114310ade88aa" "c684e64b79a2fa042fa912b70ba14cd8da0a7175c06ac6791efee57b3209da50" default)))
- '(package-selected-packages
-   (quote
-    (solarized-theme evil-magit magit fish-mode powerline powerline-theme evil-collection gruvbox-theme evil-leader centered-cursor-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
